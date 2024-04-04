57322987a6334b26e3f4bd2678298ffca6378ee2
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-04-04 09:16:10 +0200

    1st Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {BookCardComponent} from "./book-card/book-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, BookCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bookmonkey-client';
+  title = 'bookmonkey-client :)';
 }
