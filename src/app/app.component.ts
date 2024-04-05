5665d501a94a6694ee2d59dbce93cd671387e82e
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-04-05 09:51:23 +0200

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {BookCardComponent} from "./book/book-card/book-card.component";
-import {IBook} from "./book/book";
-import {BookFilterPipe} from "./book/book-filter/book-filter.pipe";
 import {BookComponent} from "./book/book/book.component";
 
 @Component({
