692f633082d64341d81267ae4af75f8f5d0fddc0
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-04-10 13:04:03 +0200

    signal

--- src/app/book/book/book.component.ts
+++ src/app/book/book/book.component.ts
@@ -1,4 +1,4 @@
-import {Component, DestroyRef, inject} from '@angular/core';
+import {Component, computed, DestroyRef, inject, Signal} from '@angular/core';
 import {IBook} from "../book";
 import {AsyncPipe, CommonModule} from "@angular/common";
 import {BookCardComponent} from "../book-card/book-card.component";
@@ -6,6 +6,7 @@ import {BookFilterPipe} from "../book-filter/book-filter.pipe";
 import {BookApiService} from "../book-api.service";
 import {Observable, Subscription} from "rxjs";
 import {Router, RouterLink} from "@angular/router";
+import {toSignal} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-book',
@@ -20,7 +21,9 @@ export class BookComponent {
   private subscription = Subscription.EMPTY
   private bookApiService = inject(BookApiService)
   private router = inject(Router);
-  books$: Observable<IBook[]> = this.bookApiService.getAll();
+  // books$: Observable<IBook[]> = this.bookApiService.getAll();
+  books: Signal<IBook[]> = toSignal(this.bookApiService.getAll(), {initialValue: []});
+  numberOfBooks: Signal<number> = computed(()=>this.books().length)
   bookSearchTerm?: string;
   goToBookDetails(book: IBook) {
     console.log(book)
