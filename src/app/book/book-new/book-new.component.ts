1104207c372930d38b813f82907e84c9ef8ce8de
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-04-10 10:01:22 +0200

    Form validation

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,26 +1,30 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {JsonPipe} from "@angular/common";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {JsonPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-book-new',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf],
   templateUrl: './book-new.component.html',
   styleUrl: './book-new.component.scss'
 })
 export class BookNewComponent {
-  formBuilder = inject(FormBuilder);
-  bookForm: FormGroup;
+  bookForm = inject(FormBuilder).group({
+    author: ['', [Validators.required]],
+    title: ['', [Validators.required]],
+    isbn: ['', [Validators.required, Validators.minLength(13)], []],
+    abstract: ['', []],
+    }
+  )
 
   constructor() {
-    this.bookForm = this.formBuilder.group({
-      title: new FormControl(''),
-      author: new FormControl(''),
-      isbn: new FormControl(''),
-      abstract: new FormControl('')
-    });
+    this.bookForm.controls.title.valueChanges.subscribe({
+      next: value => console.log(value)
+    })
   }
+
+
 
   submit() {
     console.log(this.bookForm.value);
